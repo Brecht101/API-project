d704c281d08fab9ef1d693943b9a3fc1c7e97d95
Brecht101 <r0878861@student.thomasmore.be>
2022-10-30 12:55:38 +0100

    edited api

--- app/api.py
+++ app/api.py
@@ -1,10 +1,11 @@
-#API voor users met passwoorden opgeslagen in db + hashing?
+# API voor users met passwoorden opgeslagen in db + hashing?
 
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from datetime import datetime
 
 app = FastAPI()
+
 
 class db_User(BaseModel):
     first_name: str
@@ -13,18 +14,21 @@ class db_User(BaseModel):
     password: str
     ID: int
 
+
 class User(BaseModel):
     first_name: str
     last_name: str
     password: str
-    registration_date: datetime | None = None #User zou dit nu nog kunnen invullen, zou niet mogen
-    ID: int | None = None #zelfde hier
+    registration_date: datetime | None = None  # User zou dit nu nog kunnen invullen, zou niet mogen
+    ID: int | None = None  # zelfde hier
+
 
 class Secure_User(BaseModel):
     first_name: str
     last_name: str
     registration_date: datetime
     ID: int
+
 
 user_brecht = {
     "first_name": "Brecht",
@@ -33,11 +37,12 @@ user_brecht = {
     "registration_date": datetime.now(),
     "ID": 0
 }
-users = []
-users.append(user_brecht)
+users = [user_brecht]
+
 
 @app.get("/user")
-def get_user(id: int = Query(default=0,gt=-1, description="This parameter needs the private ID of an account to show more sensitive data.")):
+def get_user(id: int = Query(gt=-1,
+                             description="This parameter needs the private ID of an account to show more sensitive data.")):
     if id < len(users):
         user = users[id]
         return user
