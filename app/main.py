6b1e640e5a9710d836ff0cd6271a75de09683db8
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 19:11:36 +0100

    db test

--- app/main.py
+++ app/main.py
@@ -28,13 +28,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users", response_model=list[schemas.UserBase])
+@app.get("/users", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users", response_model=schemas.UserBase)
+@app.get("/users", response_model=schemas.User)
 def read_user(id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=id)
     if db_user is None:
--- app/schemas.py
+++ app/schemas.py
@@ -15,13 +15,16 @@ class UserBase(BaseModel):
     first_name: str
     last_name: str
     registration_date: datetime | None = None
+    id: int | None = None
 
 
 class UserCreate(UserBase):
     password: str
 
 class User(UserBase):
-    id: int | None = None
+    first_name: str
+    last_name: str
+    registration_date: datetime | None = None
 
     class Config:
         orm_mode = True
