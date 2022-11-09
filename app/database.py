4e22f7b4d1b13349ef709701ed284d389b032623
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 14:37:08 +0100

    db test

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import databases
 
 SQLALCHEMY_DATABASE_URL = "mysql://brecht:pass@database/users"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
