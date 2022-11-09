bd6ab38902b5f4322fe1179b0d2c04be0aa1fb62
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 20:14:06 +0100

    db test

--- app/main.py
+++ app/main.py
@@ -33,9 +33,9 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/user", Query(default=None,gt=0,
-                             description="This parameter needs the private ID of an account to show more sensitive data."), response_model=schemas.User)
-def read_user(id: int, db: Session = Depends(get_db)):
+@app.get("/user", response_model=schemas.User)
+def read_user(id: int = Query(default=None,gt=0,
+                             description="This parameter needs the private ID of an account to show more sensitive data."), db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Invalid ID")
