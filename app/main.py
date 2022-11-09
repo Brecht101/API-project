fd55d647a44c1f3803b70b1c699e210041dbece0
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 18:15:43 +0100

    db test

--- app/main.py
+++ app/main.py
@@ -22,19 +22,19 @@ def get_db():
         db.close()
 
 
-@app.post("/users/create", response_model=schemas.User)
+@app.post("/users/create", response_model=schemas.UserBase)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     user.registration_date = datetime.datetime.now()
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users", response_model=list[schemas.User])
+@app.get("/users", response_model=list[schemas.UserBase])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users", response_model=schemas.User)
+@app.get("/users", response_model=schemas.UserBase)
 def read_user(id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=id)
     if db_user is None:
