9ec855bb5507260b841d54cf93c11b9d8e3864c3
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 12:36:12 +0100

    db test

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 
 from database import Base
 
@@ -11,5 +11,5 @@ class User(Base):
     first_name = Column(String)
     last_name = Column(String)
     password = Column(String)
-    registration_date = Column(Datetime) #datetime?
+    registration_date = Column(DateTime) #datetime?
     id = Column(Integer, primary_key=True, index=True)
