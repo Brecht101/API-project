0c517505f9eda33a18cf2210416355a66c3eb273
Brecht101 <r0878861@student.thomasmore.be>
2022-11-09 20:04:11 +0100

    db test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
 
 import crud
@@ -10,7 +10,7 @@ from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Brecht Voets 2CCS01's API🔥",description="Welcome to my cool user API, where I made a replica of a real website login page that stores data in a database, hashed ofcourse!")
 
 
 # Dependency
@@ -21,8 +21,7 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/users/create") #, response_model=schemas.UserBase)
+@app.post("/users/create")
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     user.registration_date = datetime.datetime.now()
     return crud.create_user(db=db, user=user)
@@ -34,9 +33,10 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/user", response_model=schemas.User)
+@app.get("/user", Query(default=None,gt=0,
+                             description="This parameter needs the private ID of an account to show more sensitive data."), response_model=schemas.User)
 def read_user(id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Invalid ID")
     return db_user
