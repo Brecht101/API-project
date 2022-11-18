eeb974d7a4ab907d7a3b498cfff57ba23c1852f3
Brecht101 <r0878861@student.thomasmore.be>
2022-11-18 14:22:45 +0100

    edited api

--- app/schemas.py
+++ app/schemas.py
@@ -7,15 +7,14 @@ from pydantic import BaseModel
 class UserBase(BaseModel):
     first_name: str
     last_name: str
-    registration_date: datetime | None = None
-    id: int | None = None
 
 
 class UserCreate(UserBase):
     password: str
 
 class User(UserBase):
-    pass
+    registration_date: datetime | None = None
+    id: int | None = None
 
     class Config:
         orm_mode = True
